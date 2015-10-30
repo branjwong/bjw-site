75515dbe5c9fc46dab81875b8f8750ed3b1886c5
branjwong <bjwong@sfu.ca>
2015-10-29 19:41:37 -0700

    added Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt){
+
+grunt.initConfig({
+
+// elm: {
+//   compile: {
+//     files: {
+//       'output-dir/': 'path/to/Source.elm',
+//       'other-output-dir/': ['path/to/sources/*.elm', 'path/to/more/*.elm']
+//     }
+//   },
+// }
+
+  elm: {
+    compile: {
+      files: {
+        'dist/js/blog.js': 'src/pages/blog.elm',
+        'dist/js/home.js': 'src/pages/home.elm',
+        'dist/js/teaching.js': 'src/pages/teaching.elm',
+        'dist/js/programming.js': 'src/pages/programming.elm'
+      }
+    }
+  }
+
+});
+
+grunt.loadNpmTasks('grunt-elm');
+
+grunt.registerTask('speak', function(){
+  console.log("I'm speaking.");
+
+});
+
+grunt.registerTask('yell', function(){
+  console.log("I'm yelling.");
+});
+
+grunt.registerTask('default', ['speak', 'yell']);
+
+}
